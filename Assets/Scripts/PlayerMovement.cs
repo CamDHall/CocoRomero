7930b3889f87833023d051c35e98b8fb88542e59
Unknown <camerondylan.h@gmail.com>
2018-10-25 17:47:14 -0500

    Correctly set positions for anchors

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public Vector2 anchorOffset;
+    public Vector3 anchorOffset;
     public float horizontalSpeed;
     public float _horizontalCooldownAmount;
     public float forceAmount;
@@ -30,8 +30,8 @@ public class PlayerMovement : MonoBehaviour {
         pieces.Remove(transform);
         signVal = 0;
 
-        pieces[0].position = (Vector2)pieces[1].position - anchorOffset;
-        pieces[pieces.Count - 1].position = (Vector2)pieces[pieces.Count - 2].position + anchorOffset;
+        pieces[0].position = pieces[1].position - anchorOffset;
+        pieces[pieces.Count - 1].position = pieces[pieces.Count - 2].position + anchorOffset;
 	}
 	
 	void Update () {
@@ -73,30 +73,74 @@ public class PlayerMovement : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if (rb.angularVelocity < maxImpactVel)
+        if (Mathf.Abs(rb.angularVelocity) > maxImpactVel && breakDelayTimer < Time.timeSinceLevelLoad)
         {
+            /// First, check if ends
+            /// Seconds, handle between hits
+            /// If it's not circle collider, has to be between
+            Transform hit = col.otherCollider.transform;
+            Transform breakingPoint;
+            hitIndex = pieces.IndexOf(hit);
+
             if (col.otherCollider.GetType() == typeof(CircleCollider2D))
             {
                 CircleCollider2D circleCol = (CircleCollider2D)col.otherCollider;
                 if (circleCol.radius == 0.4f)
                 {
-                    Transform hit = col.otherCollider.transform;
-                    Transform breakingPoint;
-                    hitIndex = pieces.IndexOf(hit);
-
                     if (hitIndex != 0)
                     {
                         breakingPoint = pieces[hitIndex - 1];
-                        hit.position = (Vector2)pieces[hitIndex - 2].position - anchorOffset;
+                        hit.position = pieces[hitIndex - 2].position - anchorOffset;
                     } else
                     {
                         breakingPoint = pieces[hitIndex + 1];
-                        hit.position = (Vector2)pieces[hitIndex + 2].position + anchorOffset;
+                        hit.position = pieces[hitIndex + 2].position + anchorOffset;
                     }
 
                     pieces.Remove(breakingPoint);
                     Destroy(breakingPoint.gameObject);
+
+                    breakDelayTimer = Time.timeSinceLevelLoad + 3;
                 }
+            }
+            else
+            {
+                List<Transform> toBeRemoved = new List<Transform>();
+
+                if (hitIndex < pieces.Count / 2)
+                {
+                    for(int i = 0; i <= hitIndex; i++)
+                    {
+                        if (pieces[i].tag == "Ball")
+                        {
+                            Vector3 currentPos = pieces[hitIndex + 1].localPosition;
+                            pieces[i].localPosition = new Vector3(currentPos.x - anchorOffset.x, 0, 0);
+                            continue;
+                        }
+                        toBeRemoved.Add(pieces[i]);
+                    }
+                } else
+                {
+                    for(int i = hitIndex; i < pieces.Count; i++)
+                    {
+                        if (pieces[i].tag == "Ball")
+                        {
+                            Vector3 currentPos = pieces[hitIndex - 1].position;
+                            pieces[i].position = new Vector3(currentPos.x - anchorOffset.x, currentPos.y, 0);
+                            continue;
+                        }
+
+                        toBeRemoved.Add(pieces[i]);
+                    } 
+                }
+
+                foreach (Transform piece in toBeRemoved)
+                {
+                    pieces.Remove(piece);
+                    Destroy(piece.gameObject);
+                }
+
+                breakDelayTimer = Time.timeSinceLevelLoad + 3;
             }
         }
     }
